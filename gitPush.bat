e8a9cbc645b83c4371e96b80054075fa0c323ca5
gaurav9969351313 <gaurav.cdac16@gmail.com>
2018-10-02 15:29:47 +0530

    message

---
+++ gitPush.bat
@@ -0,0 +1,13 @@
+:: This is Git Pusher Utility
+:: also to work it with add it in .netrc file
+
+::machine github.com
+::  login gaurav9969351313
+::  password gauravtalele*123
+
+
+git add .
+git commit -m "message"
+git push origin master
+
+pause
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
